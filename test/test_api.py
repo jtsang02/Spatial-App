b90f01bc23d70cc4fdc513d78370bd9647e7669d
Josiah Tsang <jtsang02@student.ubc.ca>
2023-12-24 02:40:39 -0800

    add test cases

--- test/test_api.py
+++ test/test_api.py
@@ -2,23 +2,71 @@ import requests
 
 ENDPOINT = "http://localhost:9000"
 
-
+# test that the endpoint is up and running
 def test_can_call_endpoint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
 
+# test that the endpoint returns the correct data when all required data is provided
 def test_can_calculate():
     payload = {    
-        "h" : 4,
-        "w" :  3,
-        "LD" : 4.23,
-        "actOpns" : 7,
-        "sprk" : True,
-        "group" : "F-2"
+        'h' : 4,
+        'w' :  3,
+        'LD' : 4.23,
+        'actOpns' : 7,
+        'sprk' : True,
+        'group' : 'F-2'
         }
     response = requests.post(ENDPOINT + "/calculate", json=payload)
-    assert response.status_code == 200
-    
     data = response.json()
+    assert response.status_code == 200
+    assert data == {
+        'actualOpenings': '58.33',
+        'unprotectedOpenings': '88.30',
+        'frr': '1 h',
+        'construction': 'Combustible / EMT / Noncombustible',
+        'cladding': 'Combustible / Noncombustible'
+    }
 
+# when missing non required data, numbers are calculated with defaults
+def test_can_calculate_with_missing_data():
+    payload = {    
+        'h' : 4,
+        'w' :  3,
+        'LD' : 4.23,
+        'sprk' : True,
+        'group' : 'F-2'
+        }
+    response = requests.post(ENDPOINT + '/calculate', json=payload)
+    data = response.json()
+    assert response.status_code == 200
+    assert data == {
+        'actualOpenings': '0.00',
+        'unprotectedOpenings': '88.30',
+        'frr': '1 h',
+        'construction': 'Combustible / EMT / Noncombustible',
+        'cladding': 'Combustible / Noncombustible'
+    }
+
+# when limiting distance is less than 1.2 m, no openings are permitted
+def test_no_openings_permitted():
+    payload = {    
+        'h' : 4,
+        'w' :  3,
+        'LD' : 1.1,
+        'actOpns' : 0,
+        'sprk' : True,
+        'group' : 'C'
+        }
+    response = requests.post(ENDPOINT + '/calculate', json=payload)
+    data = response.json()
+    assert response.status_code == 200
+    assert data == {
+        'actualOpenings': '0.00',
+        'unprotectedOpenings': '0.00',
+        'frr': '1 h',
+        'construction': 'Noncombustible',
+        'cladding': 'Noncombustible'
+    }
+    
 # run command python -m pytest

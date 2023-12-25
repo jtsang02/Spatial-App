6c30e76cedf7dce651a4a7c84797d4d1263b1f21
Josiah Tsang <jtsang02@student.ubc.ca>
2023-12-25 14:41:30 -0800

    Update validateInput.ts

--- api/middleware/validateInput.ts
+++ api/middleware/validateInput.ts
@@ -18,13 +18,30 @@ const inputParamsSchema = {
     additionalProperties: false
 };
 
-// validate the request body against the schema
 const validateInputParams = (req: Request, res: Response, next: NextFunction) => {
+    // validate the request body against the schema
     const valid = ajv.validate(inputParamsSchema, req.body);
     if (!valid) {
         const errorMessages = ajv.errors?.map(error => error.message) ?? [];
         return res.status(400).json({ errors: errorMessages });
     }
+    
+    // validate that the height and width are greater than zero
+    if (req.body.h <= 0 && req.body.w <= 0) {
+        return res.status(400).json({ errors: ["Height and width must be greater than zero."] });
+    }
+    if (req.body.h <= 0) {
+        return res.status(400).json({ errors: ["Height must be greater than zero."] });
+    }
+    if (req.body.w <= 0) {
+        return res.status(400).json({ errors: ["Width must be greater than zero."] });
+    }
+
+    // validate that the actual openings is not greater than the area
+    if (req.body.actOpns > req.body.h * req.body.w) {
+        return res.status(400).json({ errors: ["Actual openings cannot be greater than the area."] });
+    }
+
     next();
 };
 

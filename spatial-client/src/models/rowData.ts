392564cec2a9777ceb8f6478bd07d7343804ea09
Josiah Tsang <jtsang02@student.ubc.ca>
2024-01-23 03:28:48 -0800

    base table from ag-grid

---
+++ spatial-client/src/models/rowData.ts
@@ -0,0 +1,12 @@
+interface RowData {
+    mission: string;
+    company: string;
+    location: string;
+    date: string;
+    time: string;
+    rocket: string;
+    price: number;
+    successful: boolean;
+}
+
+export default RowData;
--- spatial-client/src/pages/table.tsx
+++ spatial-client/src/pages/table.tsx
@@ -1,9 +1,110 @@
+import { AgGridReact, CustomCellRendererProps } from 'ag-grid-react'; // React Grid Logic
+import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import "ag-grid-community/styles/ag-theme-balham.css"; // Secondary Theme
+import RowData from '../models/rowData';
+import { ColDef } from 'ag-grid-community';
+import { useState, useEffect, useMemo } from 'react';
+
+// Custom Cell Renderer (Display logos based on cell value)
+const CompanyLogoRenderer = (params: CustomCellRendererProps) => (
+    <span
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+      }}
+    >
+      {params.value && (
+        <img
+          alt={`${params.value} Flag`}
+          src={`https://www.ag-grid.com/example-assets/space-company-logos/${params.value.toLowerCase()}.png`}
+          style={{
+            display: 'block',
+            width: '25px',
+            height: 'auto',
+            maxHeight: '50%',
+            marginRight: '12px',
+            filter: 'brightness(1.1)',
+          }}
+        />
+      )}
+      <p
+        style={{
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {params.value}
+      </p>
+    </span>
+  );
+  
 
 const Table: React.FC = () => {
+// Row Data: The data to be displayed.
+    const [rowData, setRowData] = useState<RowData[]>([]);
+
+    // Column Definitions: Defines & controls grid columns.
+    const [colDefs] = useState<ColDef[]>([
+    {
+        field: 'mission',
+        filter: true,
+    },
+    {
+        field: 'company',
+        cellRenderer: CompanyLogoRenderer,
+    },
+    {
+        field: 'location',
+    },
+    { field: 'date' },
+    {
+        field: 'price',
+        valueFormatter: (param) => {
+        return '£' + param.value.toLocaleString();
+        },
+    },
+    { field: 'successful' },
+    { field: 'rocket' },
+    ]);
+
+    // Fetch data & update rowData state
+    useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/space-mission-data.json') // Fetch data from server
+        .then((result) => result.json()) // Convert to JSON
+        .then((rowData) => setRowData(rowData)); // Update state of `rowData`
+    }, []);
+
+    // Apply settings across all columns
+    const defaultColDef = useMemo<ColDef>(() => {
+    return {
+        filter: true,
+        editable: true,
+    };
+    }, []);
+
+    // Container: Defines the grid's theme & dimensions.
     return (
-        <div>
-            <div className="text-5xl text-blue-800 font-bold">Table</div>
-        </div>
+    <div
+        className={
+        "ag-theme-quartz-auto-dark"
+        }
+        style={{ width: '100%', height: '100%' }}
+    >
+        {/* The AG Grid component, with Row Data & Column Definition props */}
+        <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        pagination={true}
+        onCellValueChanged={(event) =>
+            console.log(`New Cell Value: ${event.value}`)
+        }
+        />
+    </div>
     );
 };
 

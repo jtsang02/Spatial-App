8a8246163f7fd0f32cb79d5e7d090b2ad724add0
Josiah Tsang <jtsang02@student.ubc.ca>
2024-01-25 01:53:11 -0800

    define rowData interface

--- spatial-client/src/models/rowData.ts
+++ spatial-client/src/models/rowData.ts
@@ -1,12 +1,19 @@
 interface RowData {
-    mission: string;
-    company: string;
-    location: string;
-    date: string;
-    time: string;
-    rocket: string;
-    price: number;
-    successful: boolean;
+    compartment: string;
+    face?: string;
+    occupancy?: string;
+    height?: number;
+    width?: number;
+    LD?: number;
+    area?: number;
+    ratio?: number;
+    actOpns?: number;
+    sprk?: boolean;
+    actualOpenings?: number;
+    unprotectedOpenings?: number;
+    frr?: string;
+    construction?: string;
+    cladding?: string;
 }
 
 export default RowData;
